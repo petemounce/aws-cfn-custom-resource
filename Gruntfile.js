5df8ea828ae46a4a49da15a07c6a0bd4672ad9c2
Cory Thomas <cory.thomas@bazaarvoice.com>
2015-09-10 14:42:44 -0500

    Updated eslint rules to match BV styling

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,23 @@
-"use strict";
+'use strict';
 
 module.exports = function (grunt) {
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        nodeunit: {
-            all: ['lib/*_test.js', '*_test.js']
-        },
-        eslint: {
-            src: ['*.js', 'lib/**/*.js'],
-            options: {
-                configFile: 'conf/eslint.yaml'
-            }
-        }
-    });
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    nodeunit: {
+      all: ['lib/*_test.js', '*_test.js']
+    },
+    eslint: {
+      src: ['*.js', 'lib/**/*.js'],
+      options: {
+        configFile: 'conf/eslint.yaml'
+      }
+    }
+  });
 
-    grunt.loadNpmTasks("gruntify-eslint");
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('gruntify-eslint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-    // Task Definitions
-    grunt.registerTask('default', ['eslint', 'nodeunit']);
+  // Task Definitions
+  grunt.registerTask('default', ['eslint', 'nodeunit']);
 };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Convenience file to require the library from the root of the repository
 module.exports = require('./lib/resource');
